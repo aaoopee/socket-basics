b30ea36995606db10b096aa8b09af1835ffc3f4d
aaoopee <antti.pelkonen@iki.fi>
2017-04-15 17:43:16 +0300

    Added support for timestamps

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,10 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
+    var momentTimestamp = moment.utc(message.timestamp);
+
     console.log('New message received: '+message.text);
-    jQuery('.messages').append('<p>'+message.text+'</p>');
+    jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('HH:mm')+'</strong> '+message.text+'</p>');
 });
 
 // Handles submitting of new message
