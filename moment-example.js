be86403a5fdda0e1b143afbc90b491914d782ec8
aaoopee <antti.pelkonen@iki.fi>
2017-05-16 18:58:05 +0300

    Add bootstrap and style for join page

---
+++ moment-example.js
@@ -0,0 +1,9 @@
+
+var moment = require('moment');
+
+var now =moment();
+
+console.log(now.format());
+console.log(now.format('H:mm'));
+console.log(now.format('X'));
+console.log(now.format('X000'));
